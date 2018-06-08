d41226106ff7aad3c0bbe05ccb4f4ce7362a4e89
ananwill <ananwill@outlook.com>
2018-06-08 17:14:08 +0800

    add testfile

---
+++ src/com/anan/demo/Demo1.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2017-2018, Thundersoft
+ * FileName: Demo1
+ * Author:   Administrator
+ * Date:     2018/6/8 17:05
+ * Description: 该类用来展示文件模板的自定义功能
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.anan.demo;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈该类用来展示文件模板的自定义功能〉
+ *
+ * @author Administrator
+ * @create 2018/6/8
+ * @since 1.0.0
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Demo1");
+    }
+}
+
---
+++ src/com/anan/demo/LiveTemplateDemo.java
@@ -0,0 +1,25 @@
+package com.anan.demo; /**
+ * Copyright (C), 2017-2018, Thundersoft
+ * FileName: com.anan.demo.LiveTemplateDemo
+ * Author:   Administrator
+ * Date:     2018/6/8 16:50
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author Administrator
+ * @create 2018/6/8
+ * @since 1.0.0
+ */
+public class LiveTemplateDemo {
+    public static void main(String[] args) {
+        System.out.println("test Msg");
+    }
+}
+
