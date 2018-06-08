9d0886624c90cc9e59e9ba34d5e85ea677e63db1
ananwill <ananwill@outlook.com>
2018-06-08 15:54:26 +0800

    Initial commit

---
+++ src/com/anan/demo/HelloWorld.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2017-2018, Thundersoft
+ * FileName: HelloWorld
+ * Author:   Administrator
+ * Date:     2018/6/8 14:52
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.anan.demo;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author Administrator
+ * @create 2018/6/8
+ * @since 1.0.0
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+
